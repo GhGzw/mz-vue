ff7ca790864a19f7231e96b005f2857cc4b2fa7b
Your Name <you@example.com>
2019-02-18 20:40:19 +0800

    基本配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,17 @@ module.exports = {
   env: {
     node: true
   },
+  // 使用的第三方的规则
   'extends': [
     'plugin:vue/essential',
     '@vue/standard'
   ],
+  // 配置自己的规则
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'semi':'off'
+
   },
   parserOptions: {
     parser: 'babel-eslint'
